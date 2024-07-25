27e8cf35898a4d7bc248ba854ddb6135072250c0
MSuleymanli <account:suleymanovmensur5@gmail.com>
2024-07-25 18:49:52 +0400

    7.25.2024

---
+++ front/app.js
@@ -0,0 +1,20 @@
+function customerShow() {
+    var x = document.getElementById("customer_submenu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function couponShow() {
+    var x = document.getElementById("coupon_submenu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  
