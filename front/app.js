02161b68589e2b1358983b84b90238a446ac3886
MSuleymanli <account:suleymanovmensur5@gmail.com>
2024-08-05 16:55:47 +0400

    hamburger menu could not be completed

--- front/app.js
+++ front/app.js
@@ -33,3 +33,40 @@ document.addEventListener('click', function(event) {
         navbarPagesShow.classList.remove('show');
     }
 }); 
+
+// kohne navbar
+// document.addEventListener('click',function(event){
+//   event.preventDefault()
+//   const hamburgerMeny=document.getElementById('navbar_hamburger_menu')
+//   console.log('as');
+  
+// })
+
+// yeni navbar
+
+const navShow = () => {
+  console.log('as');
+  
+  const burger = document.querySelector('#navbar_hamburger_menu');
+  const nav = document.querySelector('.nav-links');
+  const links = document.querySelectorAll('.nav-links li');
+  const menu = document.querySelector('.navbar_menu')
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    links.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `linksFade 0.5s ease forwards ${index / 5 + 0.2}s`;
+      }
+    });
+
+    burger.classList.toggle('close');
+    menu.classList.toggle('open');
+  });
+}
+
+
+navShow();
